7ef4ad8881b268f2cdc3cc5933c2f9e864ebeda4
aimarammayor <aimara.mmayor@gmail.com>
2023-10-11 09:23:59 +0100

    Finish lab

--- script.js
+++ script.js
@@ -35,4 +35,52 @@ function noVowels(str) {
     console.log(newStr)
 }
 
-noVowels("string")
+noVowels("HELLO")
+
+//Iteration 2 - Nickname Generator
+
+function nickname(str) {
+
+    if ("aeiou".includes(str.charAt(2))) {
+        console.log(str.slice(0, 4))
+    } else {
+        console.log(str.slice(0, 3))
+    }
+}
+nickname("Robert")
+nickname("Jeanie")
+
+// Iteration 3 - High and low
+
+
+function highAndLow(str) {
+  var nums = str.split(" ");
+  nums.sort(function (a, b) {
+    return a - b ;
+  });
+  console.log(
+   `The highest number is: ${nums[nums.length - 1]}
+  The lowest number is: ${nums[0]}`
+  );
+}
+
+highAndLow("1 2 3 4 5");
+highAndLow("1 2 -3 4 15")
+
+
+//Iteration 4 - Reverse it
+
+
+function reverse(str) {
+    // Split the str string into words using one or more spaces as the separator
+    const words = str.trim().split(/\s+/);
+    
+    // Reverse the order of words.
+    // Join the reversed words into a string with a space separator
+    const reversedWords = words.reverse().join(" ");
+  
+    console.log(reversedWords);
+  }
+  
+  reverse("Hello    World");
+  
